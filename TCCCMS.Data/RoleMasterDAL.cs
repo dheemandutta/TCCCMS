aa781faeaa1d5f1235b434b8472ee5b012b3e2e9
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-01-04 14:06:04 +0530

    ok

--- TCCCMS.Data/RoleMasterDAL.cs
+++ TCCCMS.Data/RoleMasterDAL.cs
@@ -9,34 +9,33 @@ using System.Data;
 using System.Data.SqlClient;
 using TCCCMS.Models;
 
-
 namespace TCCCMS.Data
 {
     public class RoleMasterDAL
     {
-        public int SaveUpdateGroupMaster(GroupMasterPOCO groupMaster)
+        public int SaveUpdateRoleMaster(RoleMasterPOCO pOCO)
         {
             SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["TCCCMSDBConnectionString"].ConnectionString);
             con.Open();
-            SqlCommand cmd = new SqlCommand("stpSaveUpdateGroupMaster", con);
+            SqlCommand cmd = new SqlCommand("stpSaveUpdateRoleMaster", con);
             cmd.CommandType = CommandType.StoredProcedure;
 
-            cmd.Parameters.AddWithValue("@GroupName", groupMaster.GroupName.ToString());
+            cmd.Parameters.AddWithValue("@RoleName", pOCO.RoleName.ToString());
 
             //cmd.Parameters.AddWithValue("@IsActive", crew.IsActive);
 
-            if (!String.IsNullOrEmpty(groupMaster.CreatedBy))
+            if (!String.IsNullOrEmpty(pOCO.CreatedBy))
             {
-                cmd.Parameters.AddWithValue("@CreatedBy", groupMaster.CreatedBy.ToString());
+                cmd.Parameters.AddWithValue("@CreatedBy", pOCO.CreatedBy.ToString());
             }
             else
             {
                 cmd.Parameters.AddWithValue("@CreatedBy", DBNull.Value);
             }
 
-            if (!String.IsNullOrEmpty(groupMaster.ModifiedBy))
+            if (!String.IsNullOrEmpty(pOCO.ModifiedBy))
             {
-                cmd.Parameters.AddWithValue("@ModifiedBy", groupMaster.ModifiedBy.ToString());
+                cmd.Parameters.AddWithValue("@ModifiedBy", pOCO.ModifiedBy.ToString());
             }
             else
             {
@@ -44,13 +43,13 @@ namespace TCCCMS.Data
             }
 
 
-            if (groupMaster.GroupId > 0)
+            if (pOCO.RoleId > 0)
             {
-                cmd.Parameters.AddWithValue("@GroupId", groupMaster.GroupId);
+                cmd.Parameters.AddWithValue("@RoleId", pOCO.RoleId);
             }
             else
             {
-                cmd.Parameters.AddWithValue("@GroupId", DBNull.Value);
+                cmd.Parameters.AddWithValue("@RoleId", DBNull.Value);
             }
 
             int recordsAffected = cmd.ExecuteNonQuery();
@@ -60,65 +59,13 @@ namespace TCCCMS.Data
         }
 
 
-        public List<GroupMasterPOCO> GetAllGroupMaster()
-        {
-            List<GroupMasterPOCO> prodPOList = new List<GroupMasterPOCO>();
-            List<GroupMasterPOCO> prodPO = new List<GroupMasterPOCO>();
-            DataSet ds = new DataSet();
-            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["TCCCMSDBConnectionString"].ConnectionString))
-            {
-                using (SqlCommand cmd = new SqlCommand("stpGetAllGroupMaster", con))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    con.Open();
-
-                    SqlDataAdapter da = new SqlDataAdapter(cmd);
-                    da.Fill(ds);
-                    con.Close();
-                }
-            }
-            return ConvertDataTableToGetAllGroupMasterList(ds);
-        }
-
-        private List<GroupMasterPOCO> ConvertDataTableToGetAllGroupMasterList(DataSet ds)
-        {
-            List<GroupMasterPOCO> crewtimesheetList = new List<GroupMasterPOCO>();
-            //check if there is at all any data
-            if (ds.Tables.Count > 0)
-            {
-                foreach (DataRow item in ds.Tables[0].Rows)
-                {
-                    GroupMasterPOCO groupMasterPOCO = new GroupMasterPOCO();
-
-                    //if (item["GroupId"] != System.DBNull.Value)
-                    //    groupMasterPOCO.GroupId = Convert.ToInt32(item["GroupId"].ToString());
-
-                    if (item["GroupName"] != System.DBNull.Value)
-                        groupMasterPOCO.GroupName = item["GroupName"].ToString();
-
-                    if (item["CreatedBy"] != System.DBNull.Value)
-                        groupMasterPOCO.CreatedBy = item["CreatedBy"].ToString();
-
-                    if (item["ModifiedBy"] != System.DBNull.Value)
-                        groupMasterPOCO.ModifiedBy = item["ModifiedBy"].ToString();
-
-                    //if (item["CountryID"] != System.DBNull.Value)
-                    //    groupMasterPOCO.CountryID = Convert.ToInt16(item["CountryID"]);
-
-                    crewtimesheetList.Add(groupMasterPOCO);
-                }
-            }
-            return crewtimesheetList;
-        }
-
-
-        public int DeleteGroupMaster(int GroupId)
+        public int DeleteRoleMaster(int RoleId)
         {
             SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["TCCCMSDBConnectionString"].ConnectionString);
             con.Open();
-            SqlCommand cmd = new SqlCommand("stpDeleteGroupMaster", con);
+            SqlCommand cmd = new SqlCommand("stpDeleteRoleMaster", con);
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@GroupId", GroupId);
+            cmd.Parameters.AddWithValue("@RoleId", RoleId);
 
             int recordsAffected = cmd.ExecuteNonQuery();
             DataSet ds = new DataSet();
