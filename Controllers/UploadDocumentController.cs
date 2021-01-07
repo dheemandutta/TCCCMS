8a278398e598c6e1c4c009bac78b89092623063b
Bingshu Baidya <bingshu.bk@hotmail.com>
2021-01-07 08:47:43 +0530

    drag & drop upload

---
+++ Controllers/UploadDocumentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace TCCCMS.Controllers
+{
+    public class UploadDocumentController : Controller
+    {
+        // GET: UploadDocument
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(List<HttpPostedFileBase> fileData)
+        {
+            string path = Server.MapPath("~/Uploads/");
+            foreach (HttpPostedFileBase postedFile in fileData)
+            {
+                if (postedFile != null)
+                {
+                    string fileName = Path.GetFileName(postedFile.FileName);
+                    postedFile.SaveAs(path + fileName);
+                }
+            }
+
+            return Content("Success");
+        }
+
+        [HttpPost]
+        public JsonResult SaveFiles()
+        {
+            return Json("Success", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
