1ccc890e1d8c483b3fb900a888a67abb7240818f
Bingshu Baidya <bingshu.bk@hotmail.com>
2021-01-07 20:21:27 +0530

    added UploadDocument DAL, BL, Js, Upload view

--- Controllers/UploadDocumentController.cs
+++ Controllers/UploadDocumentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using TCCCMS.Models;
+using TCCCMS.Business;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,8 +17,79 @@ namespace TCCCMS.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            UploadDocumentBL uploadBl = new UploadDocumentBL();
+            List<FormsCategory> catrgoryList = new List<FormsCategory>();
+            catrgoryList = uploadBl.GetCategoryList();
+            //List<SelectListItem> catList = new List<SelectListItem>();
+            //catList = catrgoryList.Select(c =>
+            //                                            new SelectListItem()
+            //                                            {
+            //                                                Text = c.CatecoryName,
+            //                                                Value = c.ID.ToString()
+            //                                            }).ToList();
+
+            //ViewBag.FormsCategory = catList.Insert(0,new SelectListItem { Value="0",Text="Select",Selected=true});
+            ViewBag.FormsCategory = catrgoryList.Select(c =>
+                                                        new SelectListItem()
+                                                        {
+                                                            Text = c.CatecoryName,
+                                                            Value = c.ID.ToString()
+                                                        }).ToList();
+
+            return View();
+        }
         [HttpPost]
-        public ActionResult Upload(List<HttpPostedFileBase> fileData)
+        public ActionResult UploadFiles(FormsCategory category, string categoryId, string categoryName, List<HttpPostedFileBase> fileData)
+        {
+            
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    //  Get all files from Request object  
+                    HttpFileCollectionBase files = Request.Files;
+                    var s = Request.Params.Keys;
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        //string path = AppDomain.CurrentDomain.BaseDirectory + "Uploads/";  
+                        //string filename = Path.GetFileName(Request.Files[i].FileName);  
+
+                        HttpPostedFileBase file = files[i];
+                        string fname;
+
+                        // Checking for Internet Explorer  
+                        if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                        {
+                            string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                            fname = testfiles[testfiles.Length - 1];
+                        }
+                        else
+                        {
+                            fname = file.FileName;
+                        }
+
+                        // Get the complete folder path and store the file inside it.  
+                        fname = Path.Combine(Server.MapPath("~/Uploads/"), fname);
+                        file.SaveAs(fname);
+                    }
+                    // Returns message that successfully uploaded  
+                    return Json("File Uploaded Successfully!");
+                }
+                catch (Exception ex)
+                {
+                    return Json("Error occurred. Error details: " + ex.Message);
+                }
+            }
+            else
+            {
+                return Json("No files selected.");
+            }
+        }
+        [HttpPost]
+        public ActionResult UploadDropFile(List<HttpPostedFileBase> fileData)
         {
             string path = Server.MapPath("~/Uploads/");
             foreach (HttpPostedFileBase postedFile in fileData)
---
+++ js/Upload/UploadDocument.js
@@ -0,0 +1,79 @@
+﻿function EnableFileControl(x) {
+    var catId = x.value;
+    if (catId !== "" | catId != "undefined" | catId != null) {
+        $("#fileUpload").prop('disabled', false);
+    }
+    else {
+        $("#fileUpload").prop('disabled', true);
+    }
+}
+
+function UploadFiles() {
+    //var catId = $("#drpCategory").value;
+    //var fileUpload = $("#fileUpload").get(0);
+    //var files = fileUpload.files;
+    //var fileData = new FormData();
+    //for (var i = 0; i < files.length; i++) {
+    //       fileData.append(files[i].name, files[i]);
+    // }
+
+    //$.ajax({
+    //    type: 'POST',
+    //    url: '/UploadDocument/UploadFiles',
+    //    data: JSON.stringify({ categoryId: '1', fileData: fileData }),
+    //    datatype: "json",
+    //    contentType: "application/json; charset=utf-8",
+    //    processData: false,
+    //    success: function (data) {
+    //        alert('success');
+    //    },
+    //    error: function () {
+    //        alert('error');
+    //    }
+    //});
+
+    //********--------------------------------------------------------------------------
+     //Checking whether FormData is available in browser  
+    if (window.FormData !== undefined) {
+        var catId = $("#drpCategory").val();
+        var catName = $("#drpCategory option:selected").text();
+        var fileUpload = $("#fileUpload").get(0);
+        var files = fileUpload.files;
+        // Create FormData object  
+        var fileData = new FormData();
+        var cate = {
+            ID: '2',
+            CatecoryName:'Admin'
+
+        };
+
+        // Looping over all files and add it to FormData object  
+        for (var i = 0; i < files.length; i++) {
+            fileData.append(files[i].name, files[i]);
+        }
+
+        // Adding one more key to FormData object  
+        fileData.append('category', cate);
+        fileData.append('categoryId', catId);
+        fileData.append('categoryName', catName);
+
+        $.ajax({
+            url: '/UploadDocument/UploadFiles',
+            type: "POST",
+            //datatype: "json",
+            //contentType: "application/json; charset=utf-8",
+            contentType: false, // Not to set any content header  
+            processData: false, // Not to process data  
+            data:  fileData ,
+            //data: { categoryId: y},
+            success: function (result) {
+                alert(result);
+            },
+            error: function (err) {
+                alert(err.statusText);
+            }
+        });
+    } else {
+        alert("FormData is not supported.");
+    }  
+}
