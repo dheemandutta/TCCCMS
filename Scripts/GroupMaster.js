3d951b20c12895ebbeb62539eb6bd4e275c89c05
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-01-08 13:49:38 +0530

    GroupMaster crud completed.

---
+++ Scripts/GroupMaster.js
@@ -0,0 +1,222 @@
+﻿function validate() {
+    var isValid = true;
+
+    if ($('#GroupName').val().length === 0) {
+        $('#GroupName').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#GroupName').css('border-color', 'lightgrey');
+    }
+
+    return isValid;
+}
+
+function clearTextBox() {
+    $('#GroupId').val("");
+    $('#GroupName').val("");
+    // $('#CreatedBy').val("");
+    // $('#ModifiedBy').val("");
+}
+
+function SaveUpdateGroupMaster() {
+
+    //alert($('textarea#Comments').val());
+    //debugger;
+    var posturl = $('#SaveUpdateGroupMaster').val();
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    //alert(res);
+    if (res) {
+        var GroupMaster = {
+            GroupId: $('#GroupId').val(),
+            GroupName: $('#GroupName').val()
+            //CreatedBy: $('#CreatedBy').val(),
+            //ModifiedBy: $('#ModifiedBy').val()
+        };
+        $.ajax({
+            url: posturl,
+            data: JSON.stringify(GroupMaster),
+            type: "POST",
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+
+            //success: function (result) {
+
+            //    alert('Added Successfully');
+
+            //    clearTextBox();
+            //    }         
+            //,
+            success: function (result) {
+                loadData();
+                $('#myModal').modal('hide');
+                // alert('Added Successfully');
+
+                toastr.options = {
+                    "closeButton": false,
+                    "debug": false,
+                    "newestOnTop": false,
+                    "progressBar": false,
+                    "positionClass": "toast-bottom-full-width",
+                    "preventDuplicates": false,
+                    "onclick": null,
+                    "showDuration": "300",
+                    "hideDuration": "1000",
+                    "timeOut": "5000",
+                    "extendedTimeOut": "1000",
+                    "showEasing": "swing",
+                    "hideEasing": "linear",
+                    "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                };
+
+                toastr.success("Added Successfully");
+
+                clearTextBox();
+            },
+            error: function (errormessage) {
+                console.log(errormessage.responseText);
+            }
+        });
+    }
+}
+
+function loadData() {
+    var loadposturl = $('#loaddata').val();
+    $.ajax({
+        url: loadposturl,
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            SetUpGrid();
+        },
+        error: function (errormessage) {
+            console.log(errormessage.responseText);
+        }
+    });
+}
+
+function SetUpGrid() {
+    var loadposturl = $('#loaddata').val();
+
+    //do not throw error
+    $.fn.dataTable.ext.errMode = 'none';
+
+    //check if datatable is already created then destroy iy and then create it
+    if ($.fn.dataTable.isDataTable('#GroupMasterTable')) {
+        table = $('#GroupMasterTable').DataTable();
+        table.destroy();
+    }
+
+    // alert('hh');
+    var table = $("#GroupMasterTable").DataTable({
+        "dom": 'Bfrtip',
+        "rowReorder": false,
+        "ordering": false,
+        "filter": false, // this is for disable filter (search box)
+
+        "ajax": {
+            "url": loadposturl,
+            "type": "POST",
+            "datatype": "json"
+        },
+        "columns": [
+            {
+                "data": "GroupName", "name": "GroupName", "autoWidth": true
+            },
+            //{
+            //    "data": "CreatedBy", "name": "CreatedBy", "autoWidth": true
+            //},
+            //{
+            //    "data": "ModifiedBy", "name": "ModifiedBy", "autoWidth": true
+            //},
+            {
+                "data": "GroupId", "width": "50px", "render": function (data) {
+                    return '<a href="#" class="btn btn-info btn-sm" style="background-color: #e90000;" onclick="GetGroupMasterByGroupId(' + data + ')">Edit</a>';
+                }
+            },
+            {
+                "data": "GroupId", "width": "50px", "render": function (d) {
+                    //debugger;
+                    return '<a href="#" class="btn btn-info btn-sm" style="background-color: #e90000;" onclick="DeleteGroupMaster(' + d + ')">Delete</a>';
+                }
+            }
+
+        ],
+        "rowId": "GroupId",
+        "dom": "Bfrtip"
+    });
+}
+
+
+function DeleteGroupMaster(GroupId) {
+    var e = $('#DeleteGroupMaster').val();
+    var ans = confirm("Are you sure you want to delete this Record?");
+    if (ans) {
+        // debugger;
+        $.ajax({
+            url: e,
+            data: JSON.stringify({ GroupId: GroupId }),
+            type: "POST",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function (result) {
+                // debugger;
+
+                if (result == -1) {
+                    alert("Department cannot be deleted as this is already used.");
+                }
+                else if (result == 0) {
+                    alert("Department cannot be deleted as this is already used.");
+                }
+                else {
+                    loadData();
+                }
+            },
+            error: function () {
+                alert("Department cannot be deleted as this is already used in Crew");
+            }
+        });
+    }
+}
+
+
+
+function GetGroupMasterByGroupId(GroupId) {
+    $('#GroupName').css('border-color', 'lightgrey');
+    var x = $("#GetGroupMasterByGroupId").val();
+    //alert(x);
+    //debugger;
+    $.ajax({
+        url: x,
+        data:
+        {
+            GroupId: GroupId
+        },
+        type: "GET",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            //debugger;
+            $('#GroupId').val(result.GroupId);
+            $('#GroupName').val(result.GroupName);
+            //$('#CreatedBy').val(result.CreatedBy);
+            //$('#ModifiedBy').val(result.ModifiedBy);
+
+            $('#myModal').modal('show');
+            $('#btnUpdate').show();
+            $('#btnAdd').hide();
+
+        },
+        error: function (errormessage) {
+            //debugger;
+            console.log(errormessage.responseText);
+        }
+    });
+    return false;
+}
+
--- TCCCMS.Data/GroupMasterDAL.cs
+++ TCCCMS.Data/GroupMasterDAL.cs
@@ -13,6 +13,48 @@ namespace TCCCMS.Data
 {
     public class GroupMasterDAL
     {
+
+        public List<GroupMasterPOCO> GetAllGroupMasterPageWise(int pageIndex, ref int recordCount, int length/*, int VesselID*/)
+        {
+            List<GroupMasterPOCO> pOList = new List<GroupMasterPOCO>();
+            List<GroupMasterPOCO> equipmentsPO = new List<GroupMasterPOCO>();
+
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["TCCCMSDBConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("stpGetAllGroupMasterPageWise", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@PageIndex", pageIndex);
+                    cmd.Parameters.AddWithValue("@PageSize", length);
+                    cmd.Parameters.Add("@RecordCount", SqlDbType.Int, 4);
+                    cmd.Parameters["@RecordCount"].Direction = ParameterDirection.Output;
+                    //cmd.Parameters.AddWithValue("@VesselID", VesselID);
+                    con.Open();
+
+                    DataSet ds = new DataSet();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(ds);
+                    //prodPOList = Common.CommonDAL.ConvertDataTable<ProductPOCO>(ds.Tables[0]);
+
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        pOList.Add(new GroupMasterPOCO
+                        {
+                            GroupId = Convert.ToInt32(dr["GroupId"]),
+                            GroupName = Convert.ToString(dr["GroupName"]),
+                            CreatedBy = Convert.ToString(dr["CreatedBy"]),
+                            ModifiedBy = Convert.ToString(dr["ModifiedBy"])
+                        });
+                    }
+                    recordCount = Convert.ToInt32(cmd.Parameters["@RecordCount"].Value);
+                    con.Close();
+                }
+            }
+            return pOList;
+        }
+
+
+
         public int SaveUpdateGroupMaster(GroupMasterPOCO groupMaster)
         {
             SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["TCCCMSDBConnectionString"].ConnectionString);
@@ -109,6 +151,59 @@ namespace TCCCMS.Data
             }
             return crewtimesheetList;
         }
+
+
+
+        public GroupMasterPOCO GetGroupMasterByGroupId(int GroupId)
+        {
+            GroupMasterPOCO prodPOList = new GroupMasterPOCO();
+            GroupMasterPOCO prodPO = new GroupMasterPOCO();
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["TCCCMSDBConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("stpGetGroupMasterByGroupId", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@GroupId", GroupId);
+                    con.Open();
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(ds);
+                    con.Close();
+                }
+            }
+            return ConvertDataTableToGroupMasterByGroupIdList(ds);
+        }
+
+        private GroupMasterPOCO ConvertDataTableToGroupMasterByGroupIdList(DataSet ds)
+        {
+            GroupMasterPOCO pPOCOPC = new GroupMasterPOCO();
+            //check if there is at all any data
+            if (ds.Tables.Count > 0)
+            {
+                foreach (DataRow item in ds.Tables[0].Rows)
+                {
+                    //UserMasterPOCO pPOCOPC = new UserMasterPOCO();
+
+                    if (item["GroupId"] != null)
+                        pPOCOPC.GroupId = Convert.ToInt32(item["GroupId"].ToString());
+
+                    if (item["GroupName"] != System.DBNull.Value)
+                        pPOCOPC.GroupName = item["GroupName"].ToString();
+
+                    //if (item["CreatedBy"] != System.DBNull.Value)
+                    //    pPOCOPC.CreatedBy = item["CreatedBy"].ToString();
+
+                    //if (item["ModifiedBy"] != System.DBNull.Value)
+                    //    pPOCOPC.ModifiedBy = item["ModifiedBy"].ToString();
+
+                    //pcList.Add(pPOCOPC);
+                }
+            }
+            return pPOCOPC;
+        }
+
+
 
 
         public int DeleteGroupMaster(int GroupId)
