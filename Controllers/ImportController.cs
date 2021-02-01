fbcf94c7dc5828b2bc8fd35b97fd7240ceea3f59
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-02-01 12:45:42 +0530

    01.02.2021_Full_Script_Of_TCCCMSDB (SQL Server 16)

--- Controllers/ImportController.cs
+++ Controllers/ImportController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using TCCCMS.Models;
+using TCCCMS.Business;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Text.RegularExpressions;
 
 namespace TCCCMS.Controllers
 {
---
+++ TCCCMS.Business/ImportBL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Configuration;
+//using System.Data;
+//using System.Data.SqlClient;
+using TCCCMS.Models;
+using TCCCMS.Data;
+
+namespace TCCCMS.Business
+{
+    public class ImportBL
+    {
+        public void ImportCrew(object dataTable, int vesselId)
+        {
+            ImportDAL crewImportDAL = new ImportDAL();
+            crewImportDAL.ImportCrew(dataTable, vesselId);
+        }
+    }
+}
