a5e24a161d223cacc5dd7498cb6667f637584e88
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-02-11 13:32:03 +0530

    ok

---
+++ Controllers/FormsController.cs
@@ -0,0 +1,39 @@
+﻿using TCCCMS.Models;
+using TCCCMS.Business;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TCCCMS.Controllers
+{
+    public class FormsController : Controller
+    {
+        // GET: Forms
+        public ActionResult ListOfAdministrationMmanagementForms()
+        {
+            return View();
+        }
+
+        public ActionResult ListOfMaintenanceFroms()
+        {
+            return View();
+        }
+
+        public ActionResult ListOfMmanagementOfShipPersonalForms()
+        {
+            return View();
+        }
+
+        public ActionResult NavigationMmanagementForms()
+        {
+            return View();
+        }
+        public ActionResult SafetyMmanagementForms()
+        {
+            return View();
+        }
+    }
+}
