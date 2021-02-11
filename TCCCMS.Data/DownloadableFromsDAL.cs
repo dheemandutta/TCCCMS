e665e624eb61f468f40ecafc8effd766b43c570d
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-02-11 21:52:07 +0530

    ok

---
+++ TCCCMS.Data/DownloadableFromsDAL.cs
@@ -0,0 +1,59 @@
+﻿using TCCCMS.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace TCCCMS.Data
+{
+    public class DownloadableFromsDAL
+    {
+        public List<DownloadableFroms> GetDownloadableFromsPageWise(int pageIndex, ref int recordCount, int length, int CategoryId)
+        {
+            List<DownloadableFroms> pOList = new List<DownloadableFroms>();
+            List<DownloadableFroms> equipmentsPO = new List<DownloadableFroms>();
+
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["TCCCMSDBConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("stpGetDownloadableFromsPageWise", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@PageIndex", pageIndex);
+                    cmd.Parameters.AddWithValue("@PageSize", length);
+                    cmd.Parameters.Add("@RecordCount", SqlDbType.Int, 4);
+                    cmd.Parameters["@RecordCount"].Direction = ParameterDirection.Output;
+                    cmd.Parameters.AddWithValue("@UserType", CategoryId);
+                    con.Open();
+
+                    DataSet ds = new DataSet();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(ds);
+                    //prodPOList = Common.CommonDAL.ConvertDataTable<ProductPOCO>(ds.Tables[0]);
+
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        pOList.Add(new DownloadableFroms
+                        {
+                            ID = Convert.ToInt32(dr["ID"]),
+                            FormName = Convert.ToString(dr["FormName"]),
+                            Path = Convert.ToString(dr["Path"]),
+                            Version = Convert.ToString(dr["Version"])
+                        });
+                    }
+                    recordCount = Convert.ToInt32(cmd.Parameters["@RecordCount"].Value);
+                    con.Close();
+                }
+            }
+            return pOList;
+        }
+
+
+    }
+}
---
+++ TCCCMS.Models/DownloadableFroms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCCCMS.Models
+{
+    public class DownloadableFroms
+    {
+        public int ID { get; set; }
+        public string FormName { get; set; }
+        public string Path { get; set; }
+        public string Version { get; set; }
+    }
+}
