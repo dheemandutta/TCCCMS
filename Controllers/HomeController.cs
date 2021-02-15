58cc5465aca48b601be03def75f209d113c81bc1
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-02-15 12:40:52 +0530

    ok

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -122,7 +122,7 @@ namespace TCCCMS.Controllers
                             {
                                 sb.Append("\n");
                                 //sb.Append("<li><a href='@Url.Action('" + manual.ActionName + "', '" + manual.ControllerName + "'><span class='vul'>Volume <b>" + partName + "</b> </span><span class='pgnam' style='background - color:salmon; '>" + filename + "</span></a></li>");
-                                sb.Append("<li ><a href='/" + manual.ControllerName + "/" + manual.ActionName + "' ><span class='vul'>Volume <b>" + partName + "</b> </span><span class='pgnam' style='background - color:salmon; '>" + filename + "</span></a></li>");
+                                sb.Append("<li><a href='/" + manual.ControllerName + "/" + manual.ActionName + "' ><span class='vul'>Volume <b>" + partName + "</b> </span><span class='pgnam' style='background - color:salmon; '>" + filename + "</span></a></li>");
                             }
                            
                         }
