c4433241d38b08a062a3f433e13925413a65b01c
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-02-17 21:16:15 +0530

    ok

---
+++ Controllers/RevisionHistoryController.cs
@@ -0,0 +1,20 @@
+﻿using TCCCMS.Models;
+using TCCCMS.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace TCCCMS.Controllers
+{
+    public class RevisionHistoryController : Controller
+    {
+        // GET: RevisionHistory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
