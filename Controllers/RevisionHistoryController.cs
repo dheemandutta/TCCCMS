bff613d439abdbfaa004099e573190697c33d0d8
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-02-18 01:40:18 +0530

    ok

--- Controllers/RevisionHistoryController.cs
+++ Controllers/RevisionHistoryController.cs
@@ -1,11 +1,12 @@
 ﻿using TCCCMS.Models;
 using TCCCMS.Business;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.IO;
+using System.Text.RegularExpressions;
 
 namespace TCCCMS.Controllers
 {
@@ -16,5 +17,55 @@ namespace TCCCMS.Controllers
         {
             return View();
         }
+
+        public JsonResult LoadData()
+        {
+            int draw, start, length;
+            int pageIndex = 0;
+
+            if (null != Request.Form.GetValues("draw"))
+            {
+                draw = int.Parse(Request.Form.GetValues("draw").FirstOrDefault().ToString());
+                start = int.Parse(Request.Form.GetValues("start").FirstOrDefault().ToString());
+                length = int.Parse(Request.Form.GetValues("length").FirstOrDefault().ToString());
+            }
+            else
+            {
+                draw = 1;
+                start = 0;
+                length = 500;
+            }
+
+            if (start == 0)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                pageIndex = (start / length) + 1;
+            }
+
+            RevisionHistoryBL bL = new RevisionHistoryBL(); 
+            int totalrecords = 0;
+
+            List<RevisionHistory> pocoList = new List<RevisionHistory>();
+            pocoList = bL.GetRevisionHistoryPageWise(pageIndex, ref totalrecords, length/*, int.Parse(Session["VesselID"].ToString())*/);
+            List<RevisionHistory> pList = new List<RevisionHistory>();
+            foreach (RevisionHistory pC in pocoList)
+            {
+                RevisionHistory pOCO = new RevisionHistory();
+                pOCO.ID = pC.ID;
+                pOCO.FormName = pC.FormName;
+                pOCO.ModifiedSection = pC.ModifiedSection;
+                pOCO.UpdatedOn1 = pC.UpdatedOn1;
+                pOCO.Version = pC.Version;
+
+                pList.Add(pOCO);
+            }
+
+            var data = pList;
+            return Json(new { draw = draw, recordsFiltered = totalrecords, recordsTotal = totalrecords, data = data }, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
---
+++ Scripts/RevisionHistory.js
@@ -0,0 +1,59 @@
+﻿function loadData() {
+    var loadposturl = $('#loaddata').val();
+    $.ajax({
+        url: loadposturl,
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            SetUpGrid();
+        },
+        error: function (errormessage) {
+            console.log(errormessage.responseText);
+        }
+    });
+}
+
+function SetUpGrid() {
+    var loadposturl = $('#loaddata').val();
+
+    //do not throw error
+    $.fn.dataTable.ext.errMode = 'none';
+
+    //check if datatable is already created then destroy iy and then create it
+    if ($.fn.dataTable.isDataTable('#RevisionHistoryTable')) {
+        table = $('#RevisionHistoryTable').DataTable();
+        table.destroy();
+    }
+
+    // alert('hh');
+    var table = $("#RevisionHistoryTable").DataTable({
+        "dom": 'Bfrtip',
+        "rowReorder": false,
+        "ordering": false,
+        "filter": false, // this is for disable filter (search box)
+
+        "ajax": {
+            "url": loadposturl,
+            "type": "POST",
+            "datatype": "json"
+        },
+        "columns": [
+            {
+                "data": "FormName", "name": "FormName", "autoWidth": true
+            },
+            {
+                "data": "ModifiedSection", "name": "ModifiedSection", "autoWidth": true
+            },
+            {
+                "data": "UpdatedOn1", "name": "UpdatedOn1", "autoWidth": true
+            },
+            {
+                "data": "Version", "name": "Version", "autoWidth": true
+            }
+
+        ],
+        "rowId": "ID",
+        "dom": "Bfrtip"
+    });
+}
