1070da5681999e4e8273b026fcbb9572c9c03c1c
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-03-01 20:39:49 +0530

    ok

--- Controllers/FormsAndChecklistsController.cs
+++ Controllers/FormsAndChecklistsController.cs
@@ -13,5 +13,50 @@ namespace TCCCMS.Controllers
         {
             return View();
         }
+
+        public ActionResult AdministrationAndMmgt()
+        {
+            return View();
+        }
+
+        public ActionResult ManagementOfShipPersonnel()
+        {
+            return View();
+        }
+
+        public ActionResult Maintenance()
+        {
+            return View();
+        }
+
+        public ActionResult NavigationManagement()
+        {
+            return View();
+        }
+
+        public ActionResult CargoOperation()
+        {
+            return View();
+        }
+
+        public ActionResult SafetyMmgt()
+        {
+            return View();
+        }
+
+        public ActionResult FilingSystemAndRetentionMatrix()
+        {
+            return View();
+        }
+
+
+
+
+
+        // under Section 5 - CargoOperation
+        public ActionResult CO031()
+        {
+            return View();
+        }
     }
 }
