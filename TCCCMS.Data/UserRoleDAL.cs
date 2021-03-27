f35606bdc5267a07d267df32915b7e5ae0686c98
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-03-27 18:44:19 +0530

    GetUserRoleByUserID DAL BL

---
+++ TCCCMS.Data/UserRoleDAL.cs
@@ -0,0 +1,46 @@
+﻿using TCCCMS.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace TCCCMS.Data
+{
+    public class UserRoleDAL
+    {
+        public int GetUserRoleByUserID(int UserID)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["TCCCMSDBConnectionString"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("GetUserRoleByUserID", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@UserID", UserID);
+
+            int recordsAffected = cmd.ExecuteNonQuery();
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+
+            con.Close();
+            return recordsAffected;
+        }
+
+
+
+
+
+        public int DeleteRank(int RankId/*, ref string oUTPUT*/)
+        {
+            RankDAL dAL = new RankDAL();
+            return dAL.DeleteRank(RankId/*, ref oUTPUT*/);
+        }
+
+    }
+}
