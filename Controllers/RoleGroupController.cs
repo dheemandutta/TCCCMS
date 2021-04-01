46388d3d6dae44f4b29ae5e13cf7cfd9080d489c
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-04-01 08:48:35 +0530

    RoleGroup

---
+++ Controllers/RoleGroupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TCCCMS.Controllers
+{
+    public class RoleGroupController : Controller
+    {
+        // GET: RoleGroup
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
