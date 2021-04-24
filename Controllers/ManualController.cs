5e14c5323123944c10f381e5ffa4c8f46c25dbc1
Bingshu Baidya <bingshu.bk@hotmail.com>
2021-04-24 10:57:17 +0530

    modify allvolumes xml

--- Controllers/ManualController.cs
+++ Controllers/ManualController.cs
@@ -66,7 +66,7 @@ namespace TCCCMS.Controllers
         [HttpGet]
         public JsonResult GetSearchText()
         {
-            string text = "Business";
+            string text = "";
             if (TempData["SearchText"] != null)
             {
                 text = TempData["SearchText"].ToString();
