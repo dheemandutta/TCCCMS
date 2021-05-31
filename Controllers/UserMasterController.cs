dd28a5939d074644ef8a41b80e6641c1c4a40e60
BingshuBaidya <76859289+BingshuBaidya@users.noreply.github.com>
2021-05-31 14:23:47 +0530

    Modify xml (vols,ships)

--- Controllers/UserMasterController.cs
+++ Controllers/UserMasterController.cs
@@ -523,11 +523,14 @@ namespace TCCCMS.Controllers
             //string userId = "1";
 
             string userId = Session["UserId"].ToString();
+            string shipId = Session["ShipId"].ToString();
             var n = DateTime.Now;
             fileName = Path.GetFileName(fileName);
            
             return Path.GetFileNameWithoutExtension(fileName)
                       + "_"
+                      + shipId
+                      +"-"
                       + userId
                       + "_"
                       + string.Format("{0:00}{1:00}{2:00}{3:00}{4:00}{5:00}", n.Year - 2000, n.Month, n.Day, n.Hour, n.Minute, n.Second)
