cf6edeea834c382a3b65948e558a51e05856b75b
prasenjitpauldeep <45768968+prasenjitpauldeep@users.noreply.github.com>
2021-11-02 19:05:42 +0530

    ok

--- TCCCMS.Data/ImportDAL.cs
+++ TCCCMS.Data/ImportDAL.cs
@@ -33,31 +33,31 @@ namespace TCCCMS.Data
                 for (int r = 0; r < dTable.Rows.Count; r++)
                 {
 
-                    //if (dTable.Rows[r][1].ToString() != String.Empty || dTable.Rows[r][2].ToString() != String.Empty || dTable.Rows[r][3].ToString() != String.Empty)
-                    if (dTable.Rows[r][1].ToString() != String.Empty || 
-                        dTable.Rows[r][2].ToString() != String.Empty || 
-                        dTable.Rows[r][3].ToString() != String.Empty ||
-                        dTable.Rows[r][4].ToString() != String.Empty ||
-                        dTable.Rows[r][5].ToString() != String.Empty ||
-                        dTable.Rows[r][6].ToString() != String.Empty ||
-                        dTable.Rows[r][7].ToString() != String.Empty ||
-                        dTable.Rows[r][8].ToString() != String.Empty ||
-                        dTable.Rows[r][9].ToString() != String.Empty ||
-                        dTable.Rows[r][10].ToString() != String.Empty
-                        )
+                    if (dTable.Rows[r][1].ToString() != String.Empty || dTable.Rows[r][2].ToString() != String.Empty || dTable.Rows[r][3].ToString() != String.Empty)
+                    //if (dTable.Rows[r][1].ToString() != String.Empty ||
+                    //    dTable.Rows[r][2].ToString() != String.Empty ||
+                    //    dTable.Rows[r][3].ToString() != String.Empty ||
+                    //    dTable.Rows[r][4].ToString() != String.Empty ||
+                    //    dTable.Rows[r][5].ToString() != String.Empty ||
+                    //    dTable.Rows[r][6].ToString() != String.Empty ||
+                    //    dTable.Rows[r][7].ToString() != String.Empty ||
+                    //    dTable.Rows[r][8].ToString() != String.Empty ||
+                    //    dTable.Rows[r][9].ToString() != String.Empty ||
+                    //    dTable.Rows[r][12].ToString() != String.Empty
+                    //    )
                     {
 
                         command.Parameters.Clear();
                         string CrewName = dTable.Rows[r][1].ToString();
                         string Rank = dTable.Rows[r][2].ToString();
                         ///////////////////////////////////////////////////////////////////
-                        string Nationality = dTable.Rows[r][4].ToString();
-                        string DateOfBirth = dTable.Rows[r][5].ToString();
-                        string PlaceOfBirth = dTable.Rows[r][6].ToString();
-                        string DateOfJoining = dTable.Rows[r][7].ToString();
-                        string PlaceOfJoining = dTable.Rows[r][8].ToString();
-                        string PassportNo = dTable.Rows[r][9].ToString();
-                        string ExpiryDate = dTable.Rows[r][10].ToString();
+                        //string Nationality = dTable.Rows[r][4].ToString();
+                        //string DateOfBirth = dTable.Rows[r][5].ToString();
+                        //string PlaceOfBirth = dTable.Rows[r][6].ToString();
+                        //string DateOfJoining = dTable.Rows[r][7].ToString();
+                        //string PlaceOfJoining = dTable.Rows[r][8].ToString();
+                        //string PassportNo = dTable.Rows[r][9].ToString();
+                        //string ExpiryDate = dTable.Rows[r][12].ToString();
                         ///////////////////////////////////////////////////////////////////
                         string ShipNumber = shipNumber;
                         string Email = dTable.Rows[r][3].ToString();
@@ -67,13 +67,13 @@ namespace TCCCMS.Data
                         command.Parameters.AddWithValue("@UserName", CrewName);
                         command.Parameters.AddWithValue("@RankName", Rank);
                         ///////////////////////////////////////////////////////////////////
-                        command.Parameters.AddWithValue("@xyzzzz", Nationality);
-                        command.Parameters.AddWithValue("@xyzzzz", DateOfBirth);
-                        command.Parameters.AddWithValue("@xyzzzz", PlaceOfBirth);
-                        command.Parameters.AddWithValue("@xyzzzz", DateOfJoining);
-                        command.Parameters.AddWithValue("@xyzzzz", PlaceOfJoining);
-                        command.Parameters.AddWithValue("@xyzzzz", PassportNo);
-                        command.Parameters.AddWithValue("@xyzzzz", ExpiryDate);
+                        //command.Parameters.AddWithValue("@xyzzzz", Nationality);
+                        //command.Parameters.AddWithValue("@xyzzzz", DateOfBirth);
+                        //command.Parameters.AddWithValue("@xyzzzz", PlaceOfBirth);
+                        //command.Parameters.AddWithValue("@xyzzzz", DateOfJoining);
+                        //command.Parameters.AddWithValue("@xyzzzz", PlaceOfJoining);
+                        //command.Parameters.AddWithValue("@xyzzzz", PassportNo);
+                        //command.Parameters.AddWithValue("@xyzzzz", ExpiryDate);
                         ///////////////////////////////////////////////////////////////////
                         command.Parameters.AddWithValue("@ShipNo", ShipNumber);
                         if (!string.IsNullOrEmpty(Email))
